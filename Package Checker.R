a9809659c56269f5520b6abb1b93c4040213af80
mmanolak <mmanolak@hawaii.edu>
2026-01-28 11:38:15 -1000

    Updating Checker and Installer files

--- Package Checker.R
+++ Package Checker.R
@@ -1,5 +1,5 @@
 # List of packages to install and verify
-packages <- c("tidyverse", "wooldridge", "plotly", "sf", "leaflet", "tidycensus", "readxl", "osmdata", "tigris")
+packages <- c("tidyverse", "wooldridge", "plotly", "sf", "leaflet", "tidycensus", "readxl", "osmdata", "tigris", "mice", "VIM")
 
 setwd("G:/Shared drives/School Stuff/Old Sessions/9 - Spring 2026/02 - Econ 699 (Golf Course)")
 missing_packages <- packages[!sapply(packages, requireNamespace, quietly = TRUE)]
